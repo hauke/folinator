bdb5360b3b948c0479da9fd09067c9323409db14
Hauke Mehrtens <hauke@hauke-m.de>
2012-03-06 17:57:45 +0100

    add annotations

--- app/models/lecture.rb
+++ app/models/lecture.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: lectures
+#
+#  id         :integer         not null, primary key
+#  title      :string(255)
+#  created_at :datetime        not null
+#  updated_at :datetime        not null
+#
+
 class Lecture < ActiveRecord::Base
   has_many :slidesets
 end
--- app/models/slide.rb
+++ app/models/slide.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: slides
+#
+#  id          :integer         not null, primary key
+#  filepath    :string(255)
+#  slideset_id :integer
+#  created_at  :datetime        not null
+#  updated_at  :datetime        not null
+#
+
 class Slide < ActiveRecord::Base
   belongs_to :slideset
   has_many :annotations
