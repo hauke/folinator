13988a407e30e2965491b880b15d01c7f39dcfdd
Hauke Mehrtens <hauke@hauke-m.de>
2012-03-06 18:12:50 +0100

    add generated annotations

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ SlideAnnotator::Application.routes.draw do
 
   resources :slides
 
+  resources :annotations
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
