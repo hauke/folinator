2ae35057f0fa8f3ba664471d2d2828c5cf13e2e4
Hauke Mehrtens <hauke@hauke-m.de>
2012-03-06 17:28:31 +0100

    rails project generated

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SlideAnnotator::Application.config.secret_token = '176f5bbfbdef01cabd299019d123d9a60950bf61afca041b61cc53da543f49796136a0e7d8dacc573154cf7b403723906393670253beda63add53f0f7b8a2a03'
