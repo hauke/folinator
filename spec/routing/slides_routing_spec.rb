48ed837f0b9d7483117d33d13765b7cda4f41758
DarkTigger <klager88@tzi.de>
2012-03-07 18:53:45 +0100

    fixed slide routing tests

--- spec/routing/slides_routing_spec.rb
+++ spec/routing/slides_routing_spec.rb
@@ -2,33 +2,33 @@ require "spec_helper"
 
 describe SlidesController do
   describe "routing" do
-
+#{"action"=>"index", "controller"=>"slides", "slideset_id"=>"1"}> did not match <{"controller"=>"slidesets/1/slides", "action"=>"index"}>, difference: <{"controller"=>"slidesets/1/slides", "slideset_id"=>"1"}
     it "routes to #index" do
-      get("/slides").should route_to("slides#index")
+      get("slidesets/1/slides").should route_to("slides#index", "slideset_id"=>"1")
     end
 
     it "routes to #new" do
-      get("/slides/new").should route_to("slides#new")
+      get("slidesets/1/slides/new").should route_to("slides#new", "slideset_id"=>"1")
     end
 
     it "routes to #show" do
-      get("/slides/1").should route_to("slides#show", :id => "1")
+      get("slidesets/1/slides/1").should route_to("slides#show", :id => "1", "slideset_id"=>"1")
     end
 
     it "routes to #edit" do
-      get("/slides/1/edit").should route_to("slides#edit", :id => "1")
+      get("slidesets/1/slides/1/edit").should route_to("slides#edit", :id => "1", "slideset_id"=>"1")
     end
 
     it "routes to #create" do
-      post("/slides").should route_to("slides#create")
+      post("slidesets/1/slides").should route_to("slides#create", "slideset_id"=>"1")
     end
 
     it "routes to #update" do
-      put("/slides/1").should route_to("slides#update", :id => "1")
+      put("slidesets/1/slides/1").should route_to("slides#update", :id => "1", "slideset_id"=>"1")
     end
 
     it "routes to #destroy" do
-      delete("/slides/1").should route_to("slides#destroy", :id => "1")
+      delete("slidesets/1/slides/1").should route_to("slides#destroy", :id => "1", "slideset_id"=>"1")
     end
 
   end
