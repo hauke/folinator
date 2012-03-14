2ce3ce56040b025417ab2e09d0261fc2c33c91a7
Hauke Mehrtens <hauke@hauke-m.de>
2012-03-14 18:04:07 +0100

    fix page title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,10 +2,19 @@
 module ApplicationHelper
   def full_title(page_title)
     base_title = "Folinator"
-    if page_title.empty?
-      base_title
-    else
+    if (!page_title.empty?)
       "#{base_title} | #{page_title}"
+    else if (@lecture && @slideset && @slide && @slide.title)
+      "#{base_title} | #{@lecture.title} | #{@slideset.title} | #{@slide.title.annotation}"
+    else if (@lecture && @slideset)
+      "#{base_title} | #{@lecture.title} | #{@slideset.title}"
+    else if (@lecture)
+      "#{base_title} | #{@lecture.title}"
+    else
+      base_title
+    end
+    end
+    end
     end
   end
 end
