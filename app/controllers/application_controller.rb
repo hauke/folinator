73549a6a5fff5146e2305b6b912f12a0acbecd85
Hauke Mehrtens <hauke@hauke-m.de>
2012-03-17 20:19:17 +0100

    make it possible to block users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,8 @@ class ApplicationController < ActionController::Base
   before_filter :authenticate_user!, :except => [:logout_info]
   helper_method :development_or_test_mode?
 
+  before_filter :banned?
+
 protected
 
   def development_or_test_mode?
@@ -12,6 +14,16 @@ protected
   end
 
   def is_admin
+    return false unless current_user
     current_user.admin
   end
+
+  def banned?
+    if current_user.present? && current_user.banned?
+      sign_out current_user
+      flash[:error] = "Dieser Benutzer wurde blockiert, bitte kontaktieren sie den Administrator"
+      root_path
+    end
+  end
+
 end
